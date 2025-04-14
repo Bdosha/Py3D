64b48172e9041c895fe8ee5eb26b67d6a4dfd289
Bdosha <124150146+Bdosha@users.noreply.github.com>
2025-04-14 11:50:27 +0300

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,7 @@
+from .camera import Camera
+from .light import Light
+from .object import Object
+from .scene import Scene
+from .screen import Screen
+from .utils import *
+from .player import Player
